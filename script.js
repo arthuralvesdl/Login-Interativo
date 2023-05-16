f0904f82c9eb7b2b0d31bfc352efde0e1580a7cb
Arthur <arthuralveslacerda@gmail.com>
2023-05-16 14:40:19 -0300

    Uptade
    Interação ao entrar com usuário e senha corretos

--- script.js
+++ script.js
@@ -60,21 +60,30 @@ enviar.addEventListener('click', () => {
     let user = usuario.value
     let pass = senha.value
 
-    if (user !== 'arthuralveslacerda@gmail.com' || pass !== 'Senha') {
+    if (user !== 'arthuralveslacerda@gmail.com' || pass !== 'senha') {
         boca.style.setProperty('--bocaRadius', '100px 100px 0px 0px')
         cabeca.style.transform = ' rotate(2deg)'
         setTimeout(() => {
             cabeca.style.transform = ' rotate(-2deg)'
         }, 400)
-        
+
         setTimeout(() => {
             boca.style.setProperty('--bocaRadius', '0 0 100px 100px')
             cabeca.style.transform = ' rotate(0deg)'
         }, 1500)
     } else {
         cabeca.style.transform = ' rotate(20deg)'
+        olho.style.setProperty('--HbeforeOlho', '1px')
+        pupila.style.setProperty('--HbeforePupila', '1px')
+        pupila.style.setProperty('--beforeTopPupila', '3px')
+        olho.style.setProperty('--BeforeTopOlho', '8px')
         setTimeout(() => {
             cabeca.style.transform = ' rotate(0deg)'
+
+            olho.style.setProperty('--HbeforeOlho', '20px')
+            pupila.style.setProperty('--HbeforePupila', '10px')
+            pupila.style.setProperty('--beforeTopPupila', '0px')
+            olho.style.setProperty('--BeforeTopOlho', '0px')
         }, 1000)
     }
 })
